50d08cd400bd4553891d9b9905f8646c5bdfc710
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-03-22 22:02:23 +0100

    build changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { transformWithEsbuild } from 'vite';
+import lodash from 'lodash'; 
 import restart from 'vite-plugin-restart';
 import glsl from 'vite-plugin-glsl';
 
