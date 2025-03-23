679373ba1934834aa5d44f2b6ca3e50db4d81ee7
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-03-23 09:57:27 +0100

    production vite.config changes

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,12 @@
-import react from '@vitejs/plugin-react';
-import { transformWithEsbuild } from 'vite';
-import { terser } from "rollup-plugin-terser";
-import restart from 'vite-plugin-restart';
-import glsl from 'vite-plugin-glsl';
-
+import react from '@vitejs/plugin-react'
+import { transformWithEsbuild } from 'vite'
+import restart from 'vite-plugin-restart'
+import glsl from 'vite-plugin-glsl'
 
 export default {
     root: 'src/',
     publicDir: '../public/',
     base: './',
-    define: {
-        "process.env.NODE_ENV": '"production"'
-      },
-    resolve: {
-        alias: {
-            'lottie.js': '/node_modules/three-stdlib/libs/lottie.js' // Asegura que no haya cambios inesperados
-        }
-    },
     plugins:
     [
         // Restart server on static/public file change
@@ -42,16 +32,7 @@ export default {
                 });
             },
         },
-        terser({
-            compress: {
-              unsafe: true,
-              drop_debugger: true,
-              drop_console: true,
-              evaluate: false // Desactiva la evaluación de `eval()`
-            }
-          })
     ],
-
     server:
     {
         host: true, // Open to local network and display URL
@@ -61,19 +42,6 @@ export default {
     {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true, // Add sourcemap
-        chunkSizeWarningLimit: 1500, // Aumentar el límite del aviso (opcional)
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        if (id.includes('react')) return 'vendor-react';
-                        if (id.includes('lodash')) return 'vendor-lodash';
-                        if (id.includes('three')) return 'vendor-three';
-                        return 'vendor';
-                    }
-                },
-            },
-        },
+        sourcemap: true // Add sourcemap
     },
-};
+}
