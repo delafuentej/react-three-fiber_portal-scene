0683cebe6a1c9a1c836eecb265158de52ced0aac
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-04-12 09:06:00 +0200

    backgroung root color changed

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,11 +1,19 @@
-import { OrbitControls, useGLTF, useTexture, Center, Sparkles, shaderMaterial, Environment} from '@react-three/drei';
-import { extend, useFrame, useThree } from '@react-three/fiber';
-import * as THREE from 'three';
-import { useRef} from 'react';
-import portalVertexShader from './shaders/portal/vertex.glsl';
-import portalFragmentShader from './shaders/portal/fragment.glsl';
-import WizardModel from './components/models/Wizard';
-import BattleMageWizardModel from './components/models/BattleMageWizard';
+import {
+  OrbitControls,
+  useGLTF,
+  useTexture,
+  Center,
+  Sparkles,
+  shaderMaterial,
+  Environment,
+} from "@react-three/drei";
+import { extend, useFrame, useThree } from "@react-three/fiber";
+import * as THREE from "three";
+import { useRef } from "react";
+import portalVertexShader from "./shaders/portal/vertex.glsl";
+import portalFragmentShader from "./shaders/portal/fragment.glsl";
+import WizardModel from "./components/models/Wizard";
+import BattleMageWizardModel from "./components/models/BattleMageWizard";
 
 /*
 -The model "portal" is composed of multiple parts((portal.nodes)) :(not add the whole model at once to the scene )
@@ -23,100 +31,93 @@ import BattleMageWizardModel from './components/models/BattleMageWizard';
 */
 
 const PortalMaterial = shaderMaterial(
-    {
-        uTime: 0,
-        uColorStart: new THREE.Color('#6A0DAD'),
-        uColorEnd: new THREE.Color('#4B0082') //'#0A0A5C'
-    },
-    portalVertexShader,
-    portalFragmentShader
-)
+  {
+    uTime: 0,
+    uColorStart: new THREE.Color("#6A0DAD"),
+    uColorEnd: new THREE.Color("#4B0082"), //'#0A0A5C'
+  },
+  portalVertexShader,
+  portalFragmentShader
+);
 
-extend({PortalMaterial});
+extend({ PortalMaterial });
 
-export default function Experience(){
+export default function Experience() {
+  //fog
+  const { scene } = useThree();
+  scene.fog = new THREE.FogExp2("#201c15", 0.05); //"#352f22"
 
+  const { nodes } = useGLTF("./model/portal.glb");
+  console.log("nodes", nodes);
 
-    //fog
-   const {scene} = useThree();
-   scene.fog = new THREE.FogExp2("#201c15", 0.05);//"#352f22"
+  //baked texture:
+  const bakedTexture = useTexture("./model/baked.jpg");
+  console.log("bakedTexture", bakedTexture);
+  // so the texture is immediately returned when calling useTexture, we can directly flip it:
+  bakedTexture.flipY = false; //oder in meshBasicMaterial prop => map-flipY={false}
+  bakedTexture.encoding = THREE.sRGBEncoding;
 
+  //portal ref
+  const portalMaterialRef = useRef();
 
-    const {nodes} = useGLTF('./model/portal.glb');
-    console.log('nodes', nodes)
+  // portal animation
+  useFrame((state, delta) => {
+    portalMaterialRef.current.uTime += delta * 3;
+  });
 
-    //baked texture:
-    const bakedTexture = useTexture('./model/baked.jpg');
-    console.log('bakedTexture',bakedTexture)
-    // so the texture is immediately returned when calling useTexture, we can directly flip it:
-    bakedTexture.flipY = false; //oder in meshBasicMaterial prop => map-flipY={false}
-    bakedTexture.encoding = THREE.sRGBEncoding;
-
-    //portal ref
-    const portalMaterialRef= useRef();
-
-    
-    // portal animation
-    useFrame((state, delta)=>{
-        portalMaterialRef.current.uTime += delta * 3;
-
-    });
-
-    return <>
-
+  return (
+    <>
       <color attach="background" args={["#61563f"]} />
 
-       <Environment
+      <Environment
         background
-        files='./environmentMap/vincent-mactiernan-asset.hdr'
+        files="./environmentMap/vincent-mactiernan-asset.hdr"
         ground={{
-            height: 20,//7
-            radius: 28,//28
-            scale: 50//100
-        }} 
-     
-        />
+          height: 20, //7
+          radius: 28, //28
+          scale: 50, //100
+        }}
+      />
 
-         <ambientLight intensity={3} />
+      <ambientLight intensity={3} />
 
-        <OrbitControls makeDefault />
+      <OrbitControls makeDefault />
 
-        <Center  position={[0, 0, 0]}>
+      <Center position={[0, 0, 0]}>
+        {/* floor */}
+        <mesh position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+          <planeGeometry args={[20, 20]} />
+          <meshStandardMaterial color="#61563f" transparent opacity={0} />
+        </mesh>
 
-            {/* floor */}
-            <mesh position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-                <planeGeometry args={[20, 20]} />
-                <meshStandardMaterial color="#61563f" transparent opacity={0}/>
-            </mesh>
+        {/* baked */}
+        <mesh geometry={nodes.baked.geometry}>
+          <meshBasicMaterial map={bakedTexture} />
+        </mesh>
 
-             {/* baked */}
-            <mesh geometry={nodes.baked.geometry}>
-                <meshBasicMaterial  map={bakedTexture}/>
-            </mesh>
+        {/* pole lights */}
+        <mesh
+          geometry={nodes.poleLightA.geometry}
+          position={nodes.poleLightA.position}
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
 
-            {/* pole lights */}
-            <mesh 
-                geometry={nodes.poleLightA.geometry} 
-                position={nodes.poleLightA.position}
-            >
-                 <meshBasicMaterial color='#ffffe5'/>
-            </mesh>
+        <mesh
+          geometry={nodes.poleLightB.geometry}
+          position={nodes.poleLightB.position}
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
 
-            <mesh 
-                geometry={nodes.poleLightB.geometry} 
-                position={nodes.poleLightB.position}
-            >
-                 <meshBasicMaterial color='#ffffe5'/>
-            </mesh>
-
-            <mesh 
-                geometry={nodes.portalLight.geometry}
-                position={nodes.portalLight.position}
-                rotation={nodes.portalLight.rotation}
-            >
-                   {/* portal Scene- Shader Material */}
-                   <portalMaterial ref={portalMaterialRef}/>
-                   {/* <shaderMaterial 
+        <mesh
+          geometry={nodes.portalLight.geometry}
+          position={nodes.portalLight.position}
+          rotation={nodes.portalLight.rotation}
+        >
+          {/* portal Scene- Shader Material */}
+          <portalMaterial ref={portalMaterialRef} />
+          {/* <shaderMaterial 
                         vertexShader={portalVertexShader}
                         fragmentShader={portalFragmentShader}
                         uniforms={{
@@ -126,30 +127,23 @@ export default function Experience(){
                         }}
                         
                    /> */}
+        </mesh>
 
-            </mesh>
+        {/* Sparkles- Fireflies */}
+        <Sparkles
+          count={500}
+          size={3}
+          scale={[10, 5, 10]}
+          position={[0, 0.5, 0]}
+          speed={0.4}
+        />
 
+        <WizardModel />
 
-            {/* Sparkles- Fireflies */}
-            <Sparkles
-                count={500} 
-                size={3}
-                scale={[10, 5, 10]}
-                position={[0,0,0]}
-                speed={0.4}
-                
-            />
-            
-            <WizardModel />
-              
-            <BattleMageWizardModel />     
-           
-        </Center>
-
-      
-       
-       
+        <BattleMageWizardModel />
+      </Center>
     </>
-};
+  );
+}
 
-useGLTF.preload('./model/portal.glb');
+useGLTF.preload("./model/portal.glb");
